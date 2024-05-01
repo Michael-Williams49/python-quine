a258890f3c313e5d89399bf19c26e595b2833660
Michael-Williams49 <147909653+Michael-Williams49@users.noreply.github.com>
2024-05-01 23:39:07 +0800

    Update 0_safe.py

--- 0_safe.py
+++ 0_safe.py
@@ -1,17 +1,17 @@
 import os
 
-quine = r"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
+quine = r"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id >= limit // 2: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
 
-limit = 9
+limit = 20
 id = 0
-if id * 2 >= limit: exit()
+if id >= limit // 2: exit()
 
 id *= 2
-code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
+code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id >= limit // 2: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
 open(f"{id + 1}.py", "w").write(code)
 os.system(f"python {id + 1}.py")
 
 id += 1
-code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
+code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id >= limit // 2: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
 open(f"{id + 1}.py", "w").write(code)
 os.system(f"python {id + 1}.py")
