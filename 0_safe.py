7b359832ea1b3ac9e822b460c8b3729abb2bf27a
Michael-Williams49 <147909653+Michael-Williams49@users.noreply.github.com>
2024-05-01 23:35:45 +0800

    Update 0_safe.py

--- 0_safe.py
+++ 0_safe.py
@@ -1,14 +1,17 @@
 import os
 
-quine = r"quine = r\"{quine}\"\nid = {id + 1}\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)"
+quine = r"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
+
+limit = 9
 id = 0
+if id * 2 >= limit: exit()
 
 id *= 2
-code = f"quine = r\"{quine}\"\nid = {id + 1}\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)"
+code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
 open(f"{id + 1}.py", "w").write(code)
 os.system(f"python {id + 1}.py")
 
 id += 1
-code = f"quine = r\"{quine}\"\nid = {id + 1}\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)"
+code = f"import os\n\nquine = r\"{quine}\"\n\nlimit = {limit}\nid = {id + 1}\nif id * 2 >= limit: exit()\n\nid *= 2\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")\n\nid += 1\ncode = f\"{quine}\"\nopen(f\"{{id + 1}}.py\", \"w\").write(code)\nos.system(f\"python {{id + 1}}.py\")"
 open(f"{id + 1}.py", "w").write(code)
 os.system(f"python {id + 1}.py")
